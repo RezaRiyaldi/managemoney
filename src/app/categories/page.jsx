6189b73a23b03fc61d142a5dfcb43cdad88eb206
DESKTOP-6BQ9S2T\RET001 <rezamoza3@gmail.com>
2023-08-24 14:26:08 +0700

    First Commit

---
+++ src/app/categories/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Categories() {
+  return (
+    <>
+      <div className="bg-gray-700 p-2 rounded w-8/12 mx-auto my-3 text-center uppercase">Test</div>
+      <div className='text-2xl'>Categories</div>
+    </>
+  );
+
+}
